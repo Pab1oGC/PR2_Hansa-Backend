e765f1edf4579827d9fed4d7f51d61b814912106
Pablo Gutierrez P <pablogutierrrez0@gmail.com>
2025-05-09 23:52:18 -0400

    añadiendo utilidades al proyecto

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,39 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
-
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: {
+      parser: tsparser,
+      globals: globals.node,
+    },
+    plugins: {
+      js,
+      '@typescript-eslint': tseslint,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      'no-console': 'warn',
+      semi: ['error', 'always'],
+      '@typescript-eslint/no-unused-vars': ['error'],
+      '@typescript-eslint/ban-ts-comment': 'warn',
+      'prettier/prettier': 'error',
+    },
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+    },
+    rules: {
+      'no-var': 'error',
+    },
+  },
 ]);
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
+import cors from 'cors';
+import morgan from 'morgan';
+import { logger } from './utils/logger';
 import mongoose from 'mongoose';
 import authRoutes from './routes/authRoutes';
 
@@ -8,7 +12,15 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('combined', {
+    stream: {
+      write: (message: string) => logger.info(message.trim()),
+    },
+  }));
 
 // Routes
 app.use('/api/auth', authRoutes);
@@ -18,4 +30,4 @@ mongoose
   .then(() => {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
-  .catch(err => console.error('MongoDB connection error:', err));
+  .catch((err) => console.error('MongoDB connection error:', err));
